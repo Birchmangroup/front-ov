7084477623da8b31aa38762ea04355b86028ac35
Gustavo Catalan Saavedra <gustavo.catalan@birchmangroup.com>
2016-04-29 13:12:30 -0300

    Carga inicial de archivos

---
+++ pantallas/ag-ov-wf012-home-privado/ag-ov-wf012-home-privado.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="es-CL">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+<meta charset="utf-8" />
+<title>titulo</title>
+<!--[if lt IE 9]>
+<script src="js/html5.js"></script>
+<![endif]-->
+<link rel="stylesheet" href="css/site-name.css" />
+</head>
+<body>
+
+
+
+
+
+<script src="js/jquery-1.11.0.min.js"></script>
+<script src="js/site-name.js"></script>
+<!--[if (gte IE 6)&(lte IE 8)]>
+<script src="js/nwmatcher-1-1.2.4.js"></script>
+<script  src="js/selectivizr-min.js"></script>
+<![endif]-->
+<script src="js/css_browser_selector.js"></script>
+</body>
+</html>
---
+++ plantilla-global/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="es-CL">
+<head>
+    <!--<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />-->
+    <meta charset="utf-8" />
+    <title>AGUAS - OV</title>
+    <link rel="stylesheet" href="../assets-global/global-css/aguas-ov-base.css" />
+</head>
+<body>
